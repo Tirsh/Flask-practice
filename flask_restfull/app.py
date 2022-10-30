eed9fb3e11bad3ad2e051c48423841e9aaea0c6a
Tirsh <valeriy.vasipov@gmail.com>
2022-10-30 13:04:36 +0300

    auth + item list

--- flask_restfull/app.py
+++ flask_restfull/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask("__name__")
+app.secret_key = "valeriy"
 api = Api(app)
 
 items = []
@@ -10,19 +11,26 @@ items = []
 class Item(Resource):
     @staticmethod
     def get(name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': None}, 200 if item is not None else 404
 
     @staticmethod
     def post(name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {"message": "An item with name '{}' already exists.".format(name)}
+        data = request.get_json()
         item = {'name': name,
-                'price': 12.00}
+                'price': data['price']}
         items.append(item)
         return item, 201
 
+class ItemList(Resource):
+    @staticmethod
+    def get():
+        return {'items': items}
 
+
+api.add_resource(ItemList, '/items')  # http://127.0.0.1:5000/items
 api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/item/chair
 
 app.run(debug=True)
---
+++ flask_restfull/security.py
@@ -0,0 +1,35 @@
+from user import User
+import hmac
+users = [
+    User(1, 'bob', 'qwerty'),
+]
+
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id: u for u in users}
+
+
+def safe_str_cmp(a: str, b: str) -> bool:
+    """This function compares strings in somewhat constant time. This
+    requires that the length of at least one string is known in advance.
+
+    Returns `True` if the two strings are equal, or `False` if they are not.
+    """
+
+    if isinstance(a, str):
+        a = a.encode("utf-8")  # type: ignore
+
+    if isinstance(b, str):
+        b = b.encode("utf-8")  # type: ignore
+
+    return hmac.compare_digest(a, b)
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
